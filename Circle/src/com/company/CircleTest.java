694792d9c0ab564b6a750befe541aa38201872db
DuyLeVu <levuduy132@gmail.com>
2021-10-14 15:58:37 +0700

    Add Circle

---
+++ Circle/src/com/company/CircleTest.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class CircleTest {
+    public static void main(String[] args) {
+        Circle circle = new Circle(3,"red");
+        System.out.println(circle);
+    }
+}
---
+++ Circle/src/com/company/CylinderTest.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class CylinderTest {
+    public static void main(String[] args) {
+        Cylinder cylinder = new Cylinder(3,"red",5);
+        System.out.println(cylinder);
+        System.out.println(cylinder.getVolume());
+    }
+}
